630dad3ba2ac946852fb48e0b0b3fa1e4505d664
atillatan <atillatan@gmail.com>
2017-11-08 14:07:00 +0300

    test

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,8 +1,4 @@
-var host="filehoo.com";
-if (
-(window.location.host==host || window.location.host=="www."+host) &&
-  window.location.protocol != "https:")
-window.location.protocol = "https";
+
 
 
 $('a.a-large').bind('click', function() {
