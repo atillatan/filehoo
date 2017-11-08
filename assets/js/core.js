05dc887fa14bfdc2e85cab25f1cae55bda15a97e
atillatan <atillatan@gmail.com>
2017-11-08 11:45:05 +0300

    ssl configuration

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,4 +1,9 @@
-//var element= $("div.download-movie").unbind("click").hide();
+---
+---
+
+{% if site.environment=="production" %}
+if (window.location.protocol != "https:") window.location.protocol = "https";
+{% endif %}
 
 $('a.a-large').bind('click', function() {
   console.log('something');
